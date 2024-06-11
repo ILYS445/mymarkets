18a8b1f9bdfad6097b4d67d75ae0886c16ed0095
ILYS445 <ilysmusics@gmail.com>
2024-06-12 01:36:54 +0300

    Add files via upload

---
+++ dj/constants/catalog.js
@@ -0,0 +1,63 @@
+const CATALOG = [
+    {
+        "id": "el1",
+        "name": "DJ-контроллер Pioneer DDJ-SX",
+        "img": "https://muz.by/upload/iblock/966/966b651d5027d0ab83e6ac9cb90312c0.jpg",
+        "price": 4809
+    },
+    {
+        "id": "el2",
+        "name": "DJ-контроллер Reloop Beatpad (226018)",
+        "img": "https://muz.by/upload/iblock/699/6998da0f1e9396b3e0bb31c08cb3bf28.jpg",
+        "price": 2439
+    },
+    {
+        "id": "el3",
+        "name": "Цифровая Виниловая Система Reloop Spin2+",
+        "img": "https://muz.by/upload/iblock/652/652fa95aca943602592c53c9ca027d02.jpg",
+        "price": 359
+    },
+    {
+        "id": "el4",
+        "name": "Микшерный пульт для DJ Reloop RMX-40 DSP Black Fire",
+        "img": "https://muz.by/upload/iblock/cb4/cb4ab2fd47de11648ef10096210d3e89.jpg",
+        "price": 632.70
+    },
+    {
+        "id": "el5",
+        "name": "Стойка для оборудования Athletic DJ-6",
+        "img": "https://muz.by/upload/iblock/259/2595a4eea1dcb02601dd2f73c5c194da.jpg",
+        "price": 555
+    },
+    {
+        "id": "el6",
+        "name": "Кейс для контроллера Reloop Digital Jockey PRO",
+        "img": "https://muz.by/upload/iblock/bd8/bd815ebcbe8591c847e377b2b7f5ede5.jpg",
+        "price": 364
+    },
+    {
+        "id": "el7",
+        "name": "CD-MP3 проигрыватель для DJ Pioneer MEP-7000",
+        "img": "https://muz.by/upload/iblock/927/927e512e6f60e5be0971c93c9fdda1b1.jpg",
+        "price": 2497.50
+    },
+    {
+        "id": "el8",
+        "name": "Микшерный пульт для DJ Behringer VMX-300",
+        "img": "https://muz.by/upload/iblock/e59/e592dc8f02e11069ca1873953e4a36f4.jpg",
+        "price": 1072.98
+    },
+    {
+        "id": "el9",
+        "name": "Проигрыватель виниловых дисков Reloop RP-2000 M",
+        "img": "https://muz.by/upload/iblock/b58/b58ef33f04b28979e970274e6ff81bc6.jpg",
+        "price": 1115
+    },
+    {
+        "id": "el10",
+        "name": "DJ-контроллер Reloop Contour Interface Edition",
+        "img": "https://muz.by/upload/iblock/ddf/ddfd79f221fc32cf058cd9e8a250f6ab.jpg",
+        "price": 344.09
+    }
+    
+]
