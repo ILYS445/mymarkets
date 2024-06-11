8f20d2364b390cad11598dff0bef99ddb8a6535e
ILYS445 <ilysmusics@gmail.com>
2024-06-12 01:45:07 +0300

    Add files via upload

---
+++ klavish/constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        "id": "el21",
+        "name": "Цифровое пианино Solista DP200WH",
+        "img": "https://muz.by/upload/iblock/511/solista_dp200wh_tsifrovoe_pianino.jpg",
+        "price": 1526
+    },
+    {
+        "id": "el22",
+        "name": "Синтезатор Casio Casiotone CT-S100 Black",
+        "img": "https://muz.by/upload/iblock/1cb/CT_S100_F.jpg",
+        "price": 602.46
+    },
+    {
+        "id": "el23",
+        "name": "Цифровое фортепиано Yamaha P-145B",
+        "img": "https://muz.by/upload/iblock/707/bp6mkw.jpg",
+        "price": 2773
+    },
+    {
+        "id": "el24",
+        "name": "Цифровое пианино Solista DP600WH",
+        "img": "https://muz.by/upload/iblock/6b6/qc6xar14skxdd36hisyofqxzbqasa10u.jpeg",
+        "price": 1289
+    },
+    {
+        "id": "el25",
+        "name": "Цифровое фортепиано Solista DP-45WH",
+        "img": "https://muz.by/upload/iblock/670/solista_dp_45_wh_tsifrovoe_pianino_tsvet_belyy.jpg",
+        "price": 1274.82
+    },
+    {
+        "id": "el26",
+        "name": "Рабочая станция Yamaha MODX7+",
+        "img": "https://muz.by/upload/iblock/8d8/38c2c381793642d0bd9e1df2b1fa3f71.jpg",
+        "price": 8522
+    },
+    {
+        "id": "el27",
+        "name": "Цифровое пианино Solista DP600BK",
+        "img": "https://muz.by/upload/iblock/afe/q2h08c7v9nv98ixhi34qpfcqre5dsk0g.jpeg",
+        "price": 1288.82
+    },
+    {
+        "id": "el28",
+        "name": "Цифровое пианино Kawai CA-901W",
+        "img": "https://muz.by/upload/iblock/45a/ca901_1_1451_white_67039.jpg",
+        "price": 16776
+    },
+    {
+        "id": "el29",
+        "name": "Синтезатор Casio CTK-3500",
+        "img": "https://muz.by/upload/iblock/142/1426c3d12c6eaeb22a5be768f91e26c4.jpg",
+        "price": 921.60
+    },
+    {
+        "id": "el30",
+        "name": "Цифровое пианино Roland RP-701-LA",
+        "img": "https://muz.by/upload/iblock/992/112120_RP701_LA_FRONT_F3_E.jpg",
+        "price": 7323.40
+    }
+]
