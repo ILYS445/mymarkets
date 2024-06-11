6137bc6565a46d61a1a49a64c1f8ab71e10c9225
ILYS445 <ilysmusics@gmail.com>
2024-06-12 01:48:35 +0300

    Add files via upload

---
+++ zvuk/constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        "id": "el31",
+        "name": "Акустическая система dB Technologies Opera Reevo 210",
+        "img": "https://muz.by/upload/iblock/f18/Opera_Reevo_210_Front.jpg",
+        "price": 5199
+    },
+    {
+        "id": "el32",
+        "name": "Мобильная акустическая система Acury AS-15T",
+        "img": "https://muz.by/upload/iblock/f94/315A9149.jpg",
+        "price": 999
+    },
+    {
+        "id": "el33",
+        "name": "Акустическая система HK Audio LUCAS NANO 305 FX",
+        "img": "https://muz.by/upload/iblock/d09/d09a4654e193762abe41a1685c66934c.jpg",
+        "price": 3691.98
+    },
+    {
+        "id": "el34",
+        "name": "Стойка под акустическую систему ACURY SP-40B",
+        "img": "https://muz.by/upload/iblock/995/315A9191_550x825.jpg",
+        "price": 166.50
+    },
+    {
+        "id": "el35",
+        "name": "Микшерный пульт Yamaha MG12XUK",
+        "img": "https://muz.by/upload/iblock/d50/d50794187e16d30e6f4030bfaa9060fb.jpg",
+        "price": 1868.46
+    },
+    {
+        "id": "el36",
+        "name": "Акустическая система Yamaha STAGEPAS 400BT",
+        "img": "https://muz.by/upload/iblock/42a/42a49ed46f713472f97dd3df20cddb59.jpg",
+        "price": 4069.98
+    },
+    {
+        "id": "el37",
+        "name": "Микшерный пульт Phonic Helix Board 24 Universal",
+        "img": "https://muz.by/upload/iblock/f0c/HelixBoard18Universal_side_41c7985ee0832b14ea99c765e77ea694.jpg",
+        "price": 4069
+    },
+    {
+        "id": "el38",
+        "name": "Стойка Athletic BOX-4AL",
+        "img": "https://muz.by/upload/iblock/62e/62e15cab2a6f5b57ec298f2b290a6337.jpg",
+        "price": 229.38
+    },
+    {
+        "id": "el39",
+        "name": "Микшерный пульт Allen&Heath GLD-112",
+        "img": "https://muz.by/upload/iblock/56b/56bc387f81b8f479ba21f6822447539a.jpg",
+        "price": 28416
+    },
+    {
+        "id": "el40",
+        "name": "Стойка Athletic BOX-206",
+        "img": "https://muz.by/upload/iblock/a08/a08ed697bdfeb6d71c7215dd265b654b.jpg",
+        "price": 277.50
+    }
+]
