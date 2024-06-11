cedbb97ec286740e2ff6d451622e8ae27d3408e4
ILYS445 <ilysmusics@gmail.com>
2024-06-12 01:41:16 +0300

    Add files via upload

---
+++ Guitars/constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        "id": "el11",
+        "name": "Акустическая гитара Yamaha F310 NT",
+        "img": "https://muz.by/upload/iblock/c40/c407690b7224f9e00c9da80309eab9c6.jpg",
+        "price": 644.70
+    },
+    {
+        "id": "el12",
+        "name": "Укулеле Flight NUS 310",
+        "img": "https://muz.by/upload/iblock/d2e/d2e8e323b141a509b0048a847b380d19.jpg",
+        "price": 228.96
+    },
+    {
+        "id": "el13",
+        "name": "Классическая гитара Cantades CG-C3 Natural",
+        "img": "https://muz.by/upload/iblock/f92/2.png",
+        "price": 408.18
+    },
+    {
+        "id": "el14",
+        "name": "Электрогитара Stagg SEL-STD BLK",
+        "img": "https://muz.by/upload/iblock/ba1/c007554df0cf5dfbea821616713abf30.jpg",
+        "price": 961
+    },
+    {
+        "id": "el15",
+        "name": "Акустическая гитара W81-12-WBAG-OP Parkwood",
+        "img": "https://muz.by/upload/iblock/04b/10_W81_12_WBAG_OP_800x800.jpg",
+        "price": 905.04
+    },
+    {
+        "id": "el16",
+        "name": "ТрансАкустическая гитара Yamaha FG-TA BS",
+        "img": "https://muz.by/upload/iblock/cd5/cd521b73a37c1d19c01b876679ca3716.jpg",
+        "price": 2572
+    },
+    {
+        "id": "el17",
+        "name": "Бас-гитара Stagg SBP-30 NAT",
+        "img": "https://muz.by/upload/iblock/be5/25ad2bf4cd6c84a40d1da70a27c567e2.jpg",
+        "price": 479.40
+    },
+    {
+        "id": "el18",
+        "name": "Электрогитара Ibanez GRG121DXL-WNF",
+        "img": "https://muz.by/upload/iblock/505/p_region_GRG121DXL_WNF_2Y_02_sub_3.jpg",
+        "price": 1264
+    },
+    {
+        "id": "el19",
+        "name": "Укулеле Veston KUS 15WH",
+        "img": "https://muz.by/upload/iblock/596/596398686a260770eedcccbd66a8c09f.jpg",
+        "price": 135
+    },
+    {
+        "id": "el20",
+        "name": "Электроакустическая гитара Yamaha SLG200N N",
+        "img": "https://muz.by/upload/iblock/61e/61ec3a306157b5d8a91ba8466b93dfbc.jpg",
+        "price": 4254
+    }
+]
